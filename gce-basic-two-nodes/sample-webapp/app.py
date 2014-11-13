fcd992fc1bbf24a7c06fbf27f5e446afc1c62f6a
Ilya Dmitrichenko <errordeveloper@gmail.com>
2014-11-13 14:44:49 +0000

    Trim down the sample app, so it only returns us a plain-text messsage

--- gce-basic-two-nodes/sample-webapp/app.py
+++ gce-basic-two-nodes/sample-webapp/app.py
@@ -5,19 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "\n".join([
-    	'<div style="text-align: center; font-size: 128px;">Hello World!</div>',
-    	'<div style="text-align: center; font-size: 64px;">{0}</div>\n'.format(socket.gethostname())])
-
-@app.route("/counter")
-def counter():
-	try:
-		count = redis.StrictRedis(host=os.environ.get('REDIS', "localhost")).incr("counter")
-	except:
-		count = "redis not found"
-	return "\n".join([
-    	'<div style="text-align: center; font-size: 128px;">{0}</div>'.format(count),
-    	'<div style="text-align: center; font-size: 64px;">{0}</div>\n'.format(socket.gethostname())])	
+    return "Hello, Weave!\n"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
