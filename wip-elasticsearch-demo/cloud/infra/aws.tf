d5d090fef015361321a0c1025b87ceee0f3bf3bf
Ilya Dmitrichenko <errordeveloper@gmail.com>
2014-11-28 18:56:41 +0000

    Ignore the secrete bits

--- wip-elasticsearch-demo/cloud/infra/aws.tf
+++
@@ -1,8 +0,0 @@
-// Please make sure fill this in, for more details see:
-// 1: https://www.terraform.io/docs/providers/aws/index.html
-// 2: https://console.aws.amazon.com/iam/home?#security_credential
-provider "aws" {
-    access_key = "..." // fill this
-    secret_key = "..."
-    region = "eu-west-1"
-}
