fe7b90a0e79dcd34c2f514853791136ee2929a6d
Ilya Dmitrichenko <errordeveloper@gmail.com>
2014-12-12 17:20:15 +0000

    Sort resources in logical order

--- terraform-example/infra/main.tf
+++ terraform-example/infra/main.tf
@@ -1,58 +1,3 @@
-resource "aws_security_group" "default" {
-    name = "weave"
-    description = "SSH access from anywhere"
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
-resource "aws_instance" "weave" {
-    count = 3
-    instance_type = "${var.aws_instance_type}"
-
-    ami = "${var.aws_coreos_ami}"
-
-    key_name = "${var.aws_key_name}"
-
-    user_data = "${file("cloud-config.yaml")}"
-
-    security_groups = ["${aws_security_group.default.name}"]
-
-    provisioner "file" {
-        source = "genenv.sh"
-        destination = "/tmp/genenv.sh"
-        connection {
-            user = "core"
-            key_file = "${var.aws_key_path}"
-        }
-    }
-
-    provisioner "file" {
-        source = "units"
-        destination = "/tmp/"
-        connection {
-            user = "core"
-            key_file = "${var.aws_key_path}"
-        }
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "sudo mv /tmp/units/*.service /etc/systemd/system/",
-            "sudo sh /tmp/genenv.sh aws ${count.index} '${var.weave_launch_password}' ${join(" ", google_compute_instance.weave.*.network.0.external_address)}",
-            "sudo systemctl start weave",
-        ]
-        connection {
-            user = "core"
-            key_file = "${var.aws_key_path}"
-        }
-    }
-}
-
 resource "google_compute_instance" "weave" {
     count = 3
     machine_type = "${var.gce_machine_type}"
@@ -127,4 +72,59 @@ resource "google_compute_firewall" "weave" {
     }
 
     source_ranges = ["0.0.0.0/0"]
+}
+
+resource "aws_instance" "weave" {
+    count = 3
+    instance_type = "${var.aws_instance_type}"
+
+    ami = "${var.aws_coreos_ami}"
+
+    key_name = "${var.aws_key_name}"
+
+    user_data = "${file("cloud-config.yaml")}"
+
+    security_groups = ["${aws_security_group.default.name}"]
+
+    provisioner "file" {
+        source = "genenv.sh"
+        destination = "/tmp/genenv.sh"
+        connection {
+            user = "core"
+            key_file = "${var.aws_key_path}"
+        }
+    }
+
+    provisioner "file" {
+        source = "units"
+        destination = "/tmp/"
+        connection {
+            user = "core"
+            key_file = "${var.aws_key_path}"
+        }
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo mv /tmp/units/*.service /etc/systemd/system/",
+            "sudo sh /tmp/genenv.sh aws ${count.index} '${var.weave_launch_password}' ${join(" ", google_compute_instance.weave.*.network.0.external_address)}",
+            "sudo systemctl start weave",
+        ]
+        connection {
+            user = "core"
+            key_file = "${var.aws_key_path}"
+        }
+    }
+}
+
+resource "aws_security_group" "default" {
+    name = "weave"
+    description = "SSH access from anywhere"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
