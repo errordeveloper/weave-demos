19bce9accb84409dca726e77341c38dcc872b803
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-01-09 11:34:38 +0000

    Align sparkles with basic terraform example

--- sparkles/cloud/infra/main.tf
+++ sparkles/cloud/infra/main.tf
@@ -1,68 +1,33 @@
-resource "aws_security_group" "default" {
-    name = "weave"
-    description = "SSH access from anywhere"
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
-resource "aws_instance" "weave" {
-    count = 3
-    instance_type = "${var.aws_instance_type}"
-
-    ami = "${var.aws_coreos_ami}"
-
-    key_name = "${var.aws_key_name}"
-
-    user_data = "${file("cloud-config.yaml")}"
-
-    security_groups = ["${aws_security_group.default.name}"]
-
-    provisioner "file" {
-        source = "genenv.sh"
-        destination = "/tmp/genenv.sh"
-        connection {
-            user = "core"
-            key_file = "${var.aws_key_path}"
-        }
-    }
-
-    provisioner "remote-exec" {
-        inline = [
-            "sudo sh /tmp/genenv.sh aws ${count.index} ${join(" ", google_compute_instance.weave.*.network.0.external_address)}",
-            "sudo systemctl start weave",
-            "sudo systemctl start elasticsearch spark",
-        ]
-        connection {
-            user = "core"
-            key_file = "${var.aws_key_path}"
-        }
-    }
-}
-
+// Declare and provision 3 GCE instances
 resource "google_compute_instance" "weave" {
     count = 3
+    // By default (see variables.tf), these are going to be of type 'n1-standard-1' in zone 'us-central1-a'.
     machine_type = "${var.gce_machine_type}"
     zone = "${var.gce_zone}"
 
-    name = "weave-gce-${count.index}"
+    // Ensure clear host naming scheme, which results in functional native DNS within GCE
+    name = "weave-gce-${count.index}" // => `weave-gce-{0,1,2}`
 
+    // Attach an alpha image of CoreOS as the primary disk
     disk {
         image = "${var.gce_coreos_disk_image}"
     }
 
+    // Attach to a network with some custom firewall rules and static IPs (details further down)
     network {
         source = "${google_compute_network.weave.name}"
+        address = "${element(google_compute_address.weave.*.address, count.index)}"
     }
 
+    // Provisioning
+
+    // 1. Cloud Config phase writes systemd unit definitions and only starts two host-independent units —
+    // `pre-fetch-container-images.service` and `install-weave.service`
     metadata {
         user-data = "${file("cloud-config.yaml")}"
     }
 
+    // 2. Upload shell script that generates host-specific environment file to be used by `weave.service`
     provisioner "file" {
         source = "genenv.sh"
         destination = "/tmp/genenv.sh"
@@ -72,9 +37,12 @@ resource "google_compute_instance" "weave" {
         }
     }
 
+    // 3. Run the `genenv.sh` script
+    // 4. Start `weave.service`
+    // 5. Start `elasticsearch.service` and `spark.service`
     provisioner "remote-exec" {
         inline = [
-            "sudo sh /tmp/genenv.sh gce ${count.index}",
+            "sudo sh /tmp/genenv.sh gce ${count.index} '${var.weave_launch_password}'",
             "sudo systemctl start weave",
             "sudo systemctl start elasticsearch spark",
         ]
@@ -85,11 +53,13 @@ resource "google_compute_instance" "weave" {
     }
 }
 
+// Custom GCE network declaration, so we can set firewall rules below
 resource "google_compute_network" "weave" {
-    name = "default"
-    ipv4_range = "10.240.0.0/16"
+    name = "weave"
+    ipv4_range = "10.220.0.0/16"
 }
 
+// Firewall rules for the network (allow inbound ssh and weave connections)
 resource "google_compute_firewall" "weave" {
     name = "ports"
     network = "${google_compute_network.weave.name}"
@@ -109,4 +79,102 @@ resource "google_compute_firewall" "weave" {
     }
 
     source_ranges = ["0.0.0.0/0"]
+}
+
+// Allocate static IPs for each of the instances, so if reboots occur
+// the AWS nodes can rejoin the weave network
+resource "google_compute_address" "weave" {
+    count = 3
+    name = "weave-gce-${count.index}-addr"
+}
+
+// Declare and provision 3 AWS instances
+resource "aws_instance" "weave" {
+    count = 3
+    // By default (see variables.tf), these are going to be of type 'm3.large' in region 'eu-west-1'.
+    instance_type = "${var.aws_instance_type}"
+
+    // Use an alpha image of CoreOS
+    ami = "${var.aws_coreos_ami}"
+
+    // Set the SSH key name to use (default: "terrraform")
+    key_name = "${var.aws_key_name}"
+
+    // Provisioning (mostly identical to the GCE counter-part)
+
+    user_data = "${file("cloud-config.yaml")}"
+
+    depends_on = [ "aws_vpc.weave", "aws_subnet.weave", "aws_security_group.weave", "aws_internet_gateway.weave" ]
+    security_groups = [ "${aws_security_group.weave.id}" ]
+    subnet_id = "${aws_subnet.weave.id}"
+    associate_public_ip_address = true
+
+    provisioner "file" {
+        source = "genenv.sh"
+        destination = "/tmp/genenv.sh"
+        connection {
+            user = "core"
+            key_file = "${var.aws_key_path}"
+        }
+    }
+
+    // The only difference here is what arguments are passed to `genenv.sh`
+    provisioner "remote-exec" {
+        inline = [
+            "sudo sh /tmp/genenv.sh aws ${count.index} '${var.weave_launch_password}' ${join(" ", google_compute_instance.weave.*.network.0.external_address)}",
+            "sudo systemctl start weave",
+            "sudo systemctl start elasticsearch spark",
+        ]
+        connection {
+            user = "core"
+            key_file = "${var.aws_key_path}"
+        }
+    }
+}
+
+// Create a VPC for Terraform to manage, so it doesn't mess with the default one
+// NOTE: this make configuration a little more complex to read, but it's just that
+// I have found some issues with creating and destroying resources on default VPC,
+// so I decided to make it safer with dedicating a VPC to Terraform.
+// TODO: refactor this as a module
+resource "aws_vpc" "weave" {
+    cidr_block = "10.220.0.0/16"
+}
+
+resource "aws_internet_gateway" "weave" {
+    vpc_id = "${aws_vpc.weave.id}"
+}
+
+resource "aws_route_table_association" "weave" {
+    subnet_id = "${aws_subnet.weave.id}"
+    route_table_id = "${aws_route_table.weave.id}"
+}
+
+resource "aws_route_table" "weave" {
+    vpc_id = "${aws_vpc.weave.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.weave.id}"
+    }
+}
+
+// With a non-default VPC we have to create a subnet also
+resource "aws_subnet" "weave" {
+    vpc_id = "${aws_vpc.weave.id}"
+    cidr_block = "10.220.1.0/24"
+    map_public_ip_on_launch = true
+}
+
+// Firewall rules for our security group only need to allow inbound ssh connections
+resource "aws_security_group" "weave" {
+    name = "weave"
+    description = "SSH access from anywhere"
+    vpc_id = "${aws_vpc.weave.id}"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
--- sparkles/cloud/infra/outputs.tf
+++ sparkles/cloud/infra/outputs.tf
@@ -1,7 +1,7 @@
-output "aws_instances" {
-    value = "AWS instances:\n - ${join("\n - ", aws_instance.weave.*.public_dns)}"
-}
+#output "aws_instances" {
+#    value = "AWS instances:\n - ${join("\n - ", aws_instance.weave.*.public_ip)}"
+#}
 
-output "gce_instances" {
-    value = "GCE instances:\n - ${join("\n - ", google_compute_instance.weave.*.network.0.external_address)}"
-}
+#output "gce_instances" {
+#    value = "GCE instances:\n - ${join("\n - ", google_compute_instance.weave.*.network.0.external_address)}"
+#}
