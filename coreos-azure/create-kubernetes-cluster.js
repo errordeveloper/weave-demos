9af8b0f66d429cb029af124f7ded2f3ba33418d3
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-02-09 16:24:58 +0000

    Add etcd for Kubernetes

---
+++ coreos-azure/create-kubernetes-cluster.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+var _ = require('underscore');
+
+var util = require('./util.js');
+
+var coreos_image_ids = {
+  'stable': '2b171e93f07c4903bcad35bda10acf22__CoreOS-Stable-522.6.0',
+};
+
+var node_count = 3;
+
+var nodes = {
+  'etcd': 3,
+}
+
+util.write_std_cluster_cloud_config(node_count);
+
+var vm_name_arg = _.template("--vm-name=<%= name %>")
+var vm_ssh_port = _.template("--ssh=<%= port %>")
+
+var initial_tasks = [
+  ['network', 'vnet', 'create',
+    '--location=West Europe',
+    '--address-space=172.16.0.0',
+    'weave-kubernetes-cluster-internal-vnet-1',
+  ],
+];
+
+var vm_create_base_args = [
+  'vm', 'create',
+  '--location=West Europe',
+  '--connect=weave-kubernetes-cluster-service-1',
+  '--virtual-network-name=weave-kubernetes-cluster-internal-vnet-1',
+  '--no-ssh-password',
+  '--ssh-cert=../azure-linux/coreos/cluster/ssh-cert.pem',
+];
+
+var etcd_cloud_config_files = util.write_kube_etcd_cloud_config(nodes.etcd);
+
+var main_tasks = _(nodes.etcd).times(function (n) {
+  return vm_create_base_args.concat([
+    '--custom-data=kubernetes-etcd-nodes.yml', // + etcd_cloud_config_files[n],
+    coreos_image_ids['stable'], 'core',
+    vm_name_arg({ name: util.hostname(n, 'kube-etcd') }),
+    vm_ssh_port({ port: 2200 + n }),
+  ]);
+});
+
+util.run_task_queue(initial_tasks.concat(main_tasks));
--- coreos-azure/util.js
+++ coreos-azure/util.js
@@ -4,6 +4,8 @@ _.mixin(require('underscore.string').exports());
 var fs = require('fs');
 var cp = require('child_process');
 
+var yaml = require('js-yaml');
+
 var weave_salt = function make_weave_salt () {
   var crypto = require('crypto');
   var shasum = crypto.createHash('sha256');
@@ -11,25 +13,33 @@ var weave_salt = function make_weave_salt () {
   return shasum.digest('hex');
 }();
 
-exports.hostname = function hostname (n) {
+exports.hostname = function hostname (n, prefix) {
   return _.template("<%= pre %>-<%= seq %>")({
-    pre: 'core',
+    pre: prefix || 'core',
     seq: _.pad(n, 2, '0'),
   });
 };
 
-var write_cloud_config_data = function (env_files) {
+var write_cloud_config_from_object = function (cloud_config, output_file) {
   try {
-    var yaml = require('js-yaml');
-    var cloud_config = yaml.safeLoad(fs.readFileSync('./weave-cluster.yml', 'utf8'));
-    cloud_config.write_files = env_files;
-    fs.writeFileSync('./cloud-config.yml', [
+    fs.writeFileSync(output_file, [
       '#cloud-config',
       yaml.safeDump(cloud_config),
     ].join("\n"));
   } catch (e) {
     console.log(e);
   }
+};
+
+var write_std_cloud_config_data = function (env_files) {
+  var cloud_config = {};
+  try {
+    cloud_config = yaml.safeLoad(fs.readFileSync('./weave-cluster.yml', 'utf8'));
+  } catch (e) {
+    console.log(e);
+  }
+  cloud_config.write_files = env_files;
+  write_cloud_config_from_object(cloud_config, './cloud-config.yml');
 };
 
 exports.write_std_cluster_cloud_config = function (node_count) {
@@ -67,7 +77,55 @@ exports.write_std_cluster_cloud_config = function (node_count) {
     return env_file;
   };
 
-  write_cloud_config_data(_(node_count).times(make_node_config));
+  write_std_cloud_config_data(_(node_count).times(make_node_config));
+};
+
+exports.write_kube_etcd_cloud_config = function (node_count) {
+
+  var cloud_config = {
+    "coreos": {
+      "etcd": {
+        "name":"etcd",
+        "addr":"$private_ipv4:4001",
+        "bind-addr":"0.0.0.0",
+        "peer-addr":"$private_ipv4:7001",
+        "snapshot":true,
+        "max-retry-attempts":50,
+      },
+      "units": [{
+          "name":"etcd.service",
+          "command":"start",
+      }],
+      "update": {
+        "group":"stable",
+        "reboot-strategy":"off",
+      },
+    },
+  };
+
+  /*
+  require('http').get("http://discovery.etcd.io/new", function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function(data) {
+      cloud_config.coreos.etcd.discovery = data;
+      write_cloud_config_from_object(node_cloud_config, './kubernetes-etcd-nodes.yml');
+    });
+  });
+  */
+
+  var elected_node = 0;
+
+  return _(node_count).times(function (n) {
+    var node_cloud_config = _.clone(cloud_config);
+    output_file = './kube-cluster-etcd-' + n + '.yaml';
+    if (n !== elected_node) {
+      node_cloud_config.coreos.etcd.peers = [
+        exports.hostname(elected_node, 'kube-etcd'), 7001
+      ].join(':');
+    }
+    write_cloud_config_from_object(node_cloud_config, output_file);
+    return output_file;
+  });
 };
 
 exports.run_task_queue = function (given_tasks) {
