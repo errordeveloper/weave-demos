9613658593eeb824279acb67b3a16e5432d0de0e
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-02-09 12:00:48 +0000

    Add destroyer script

---
+++ coreos-azure/delete-cluster.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var _ = require('underscore');
+
+var util = require('./util.js');
+
+var node_count = 3;
+
+var delete_vms = _(node_count).times(function (n) {
+  return ['vm', 'delete', '--quiet', util.hostname(n)];
+});
+
+var delete_vnet = [
+  ['network', 'vnet', 'delete', '--quiet', 'weave-cluster-internal-vnet-1'],
+];
+
+util.run_task_queue(delete_vms.concat(delete_vnet));
