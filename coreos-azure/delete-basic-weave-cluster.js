c5cbbb0562f69a850b53b8210674b7c2414c6304
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-02-10 17:10:03 +0000

    Also wipe the storage

--- coreos-azure/delete-basic-weave-cluster.js
+++ coreos-azure/delete-basic-weave-cluster.js
@@ -7,7 +7,7 @@ var util = require('./util.js');
 var node_count = 3;
 
 var delete_vms = _(node_count).times(function (n) {
-  return ['vm', 'delete', '--quiet', util.hostname(n)];
+  return ['vm', 'delete', '--quiet', '--blob-delete', util.hostname(n)];
 });
 
 var delete_vnet = [
