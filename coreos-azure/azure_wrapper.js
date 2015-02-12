a9f528f785ccb362cdeb9829ff5b3b50c7d08695
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-02-12 09:12:23 +0000

    Add random strings in all generated file names

--- coreos-azure/azure_wrapper.js
+++ coreos-azure/azure_wrapper.js
@@ -32,12 +32,10 @@ var rand_string = function () {
 
 var weave_salt = rand_string();
 
-var generate_azure_resource_strings = function (prefix) {
-  var rand_suffix = rand_string().substring(50);
-  return {
-    vnet: [prefix, 'internal-vnet', rand_suffix].join('-'),
-    service: [prefix, 'service-cluster', rand_suffix].join('-'),
-  }
+var rand_suffix = rand_string().substring(50);
+
+var join_output_file_path = function(prefix, suffix) {
+  return './output/' + [prefix, rand_suffix, suffix].join('_');
 };
 
 var hostname = function hostname (n, prefix) {
@@ -97,7 +95,7 @@ var ipv4 = function (ocets, prefix) {
 
 var write_basic_weave_cluster_cloud_config = function (env_files) {
   var input_file = './cloud_config_templates/basic-weave-cluster-template.yml';
-  var output_file = './output/basic-weave-cluster-generated.yml';
+  var output_file = join_output_file_path('basic-weave-cluster', 'generated.yml');
 
   return process_cloud_config_template(input_file, output_file, function(cloud_config) {
     cloud_config.write_files = env_files;
@@ -125,7 +123,7 @@ exports.create_kube_etcd_cloud_config = function (node_count) {
   var input_file = './cloud_config_templates/kubernetes-cluster-etcd-node-template.yml';
 
   return _(node_count).times(function (n) {
-    var output_file = './output/kubernetes-cluster-etcd-node-' + n + '-generated.yml';
+    var output_file = join_output_file_path('kubernetes-cluster-etcd-node-' + n, 'generated.yml');
 
     return process_cloud_config_template(input_file, output_file, function(cloud_config) {
       if (n !== elected_node) {
@@ -142,7 +140,7 @@ exports.create_kube_node_cloud_config = function (node_count) {
   var elected_node = 0;
 
   var input_file = './cloud_config_templates/kubernetes-cluster-main-nodes-template.yml';
-  var output_file = './output/kubernetes-cluster-main-nodes-generated.yml';
+  var output_file = join_output_file_path('kubernetes-cluster-main-nodes', 'generated.yml');
 
   var make_node_config = function (n) {
     return generate_environment_file_entry_from_object(hostname(n, 'kube'), {
@@ -203,7 +201,7 @@ exports.run_task_queue = function (dummy) {
 };
 
 var save_state = function () {
-  var file_name = './output/' + conf.name + '_deployment.yml';
+  var file_name = join_output_file_path(conf.name, 'deployment.yml');
   try {
     conf.hosts = hosts.collection;
     fs.writeFileSync(file_name, yaml.safeDump(conf));
@@ -229,10 +227,11 @@ var create_ssh_key = function (prefix) {
     nodes: true,
     newkey: 'rsa:2048',
     subj: '/O=Weaveworks, Inc./L=London/C=GB/CN=weave.works',
-    keyout: './output/' + prefix + '.key',
-    out: './output/' + prefix + '.pem',
+    keyout: join_output_file_path(prefix, 'ssh.key'),
+    out: join_output_file_path(prefix, 'ssh.pem'),
   };
   openssl.qExec('req', opts);
+  fs.chmodSync(opts.keyout, '0600');
   console.log('Created new SSH key.');
   return {
     key: opts.keyout,
@@ -241,7 +240,7 @@ var create_ssh_key = function (prefix) {
 }
 
 var create_ssh_conf = function () {
-  var file_name = './output/' + conf.name + '_ssh_conf';
+  var file_name = join_output_file_path(conf.name, 'ssh_conf');
   var ssh_conf_head = [
     "Host *",
     "\tHostname " + conf.resources['service'] + ".cloudapp.net",
@@ -313,9 +312,12 @@ exports.create_config = function (name, nodes) {
   conf = {
     name: name,
     nodes: nodes,
-    resources: generate_azure_resource_strings(name),
+    resources: {
+      vnet: [name, 'internal-vnet', rand_suffix].join('-'),
+      service: [name, 'service-cluster', rand_suffix].join('-'),
+      ssh_key: create_ssh_key(name + rand_suffix),
+    }
   };
-  conf.resources.ssh_key = create_ssh_key(name);
 
 };
 
