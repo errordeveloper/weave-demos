56b01eb56c0f8f5fd782dc2b69b1719c2c19a494
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-02-18 16:43:09 +0000

    Load `config-override.rb` if present

--- sparkles/config.rb
+++ sparkles/config.rb
@@ -3,3 +3,8 @@ if ENV['WEAVE_PASSWORD'] and ENV['WEAVE_PEERS'] then
 else
   require File.join(File.dirname(__FILE__), "local-cluster.rb")
 end
+
+begin
+  require File.join(File.dirname(__FILE__), 'config-override.rb')
+rescue LoadError => e
+end
