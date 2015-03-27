d5083782e20fc9307d4af6e60c8bab9ffadc7ebb
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-03-27 11:56:57 +0000

    Improve subtree pull helper a bit

--- coreos-vagrant_subtree_pull.sh
+++ coreos-vagrant_subtree_pull.sh
@@ -1,1 +1,2 @@
-git subtree pull --prefix=coreos-vagrant https://github.com/coreos/coreos-vagrant master
+git ls-remote https://github.com/coreos/coreos-vagrant master \
+  | awk '{ system( "git subtree pull --message=\"Subtree merge of coreos/coreos-vagrant@" $1 "\" --prefix=coreos-vagrant https://github.com/coreos/coreos-vagrant " $1) }'
