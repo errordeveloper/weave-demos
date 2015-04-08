5377cdf19ebef32a895f73e6cbea8acea0bbc3ba
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-04-08 18:05:25 +0100

    Also make sure the user sees private IPs

--- terraform-example/infra/outputs.tf
+++ terraform-example/infra/outputs.tf
@@ -1,7 +1,15 @@
-output "aws_instances" {
+output "aws_instances_external" {
     value = "[ ${join(", ", aws_instance.weave.*.public_ip)} ]"
 }
 
-output "gce_instances" {
+output "gce_instances_external" {
     value = "[ ${join(", ", google_compute_instance.weave.*.network.0.external_address)} ]"
+}
+
+output "aws_instances_internal" {
+    value = "[ ${join(", ", aws_instance.weave.*.private_ip)} ]"
+}
+
+output "gce_instances_internal" {
+    value = "[ ${join(", ", google_compute_instance.weave.*.network.0.internal_address)} ]"
 }
