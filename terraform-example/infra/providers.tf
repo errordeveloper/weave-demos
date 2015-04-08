c9d419004ccc10bd46bea50e5b781f93090fe789
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-04-08 13:29:34 +0100

    Update for Terraform 0.4.0

--- terraform-example/infra/providers.tf
+++ terraform-example/infra/providers.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 provider "google" {
     account_file = "${var.gce_account_file}"
-    client_secrets_file = "${var.gce_client_secrets_file}"
+    # client_secrets_file = "${var.gce_client_secrets_file}" # not needed since 0.4.0
     project = "${var.gce_project_name}"
     region = "${var.gce_region}"
 }
