16f4b2b5292f3c359206e49437bfd1fe546b5831
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-05-05 18:00:45 +0100

    Use `$DOCKER_CLIENT_ARGS`

--- quartet/scripts/weave-run-on-swarm.sh
+++ quartet/scripts/weave-run-on-swarm.sh
@@ -1,15 +1,10 @@
 #!/bin/sh -xe
 
+source $(git rev-parse --show-toplevel)/quartet/scripts/defaults.sh
+
 weave_cidr=$1
 shift 1
 
 c=$(docker run -d --dns=172.17.42.1 $@)
 
-#$WEAVE attach ${weave_cidr} ${c}
-
-docker run --rm --privileged --net=host \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /proc:/hostproc \
-  -e PROCFS=/hostproc \
-  -e "affinity:container==${c}" \
-  weaveworks/weaveexec:0.10.0 --local attach ${weave_cidr} ${c}
+env DOCKER_CLIENT_ARGS="-e affinity:container==${c}" $WEAVE attach ${weave_cidr} ${c}
