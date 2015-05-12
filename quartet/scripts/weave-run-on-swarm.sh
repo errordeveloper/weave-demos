fdcd7aac72d0d72abbb1640f9d6373f840eb94a5
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-05-12 17:06:42 +0100

    March upstream change

--- quartet/scripts/weave-run-on-swarm.sh
+++ quartet/scripts/weave-run-on-swarm.sh
@@ -10,4 +10,4 @@ shift 1
 
 c=$(docker run -d --dns=172.17.42.1 $@)
 
-env DOCKER_CLIENT_ARGS="-e affinity:container==${c}" $WEAVE attach ${weave_cidr} ${c}
+env WEAVEEXEC_DOCKER_ARGS="-e affinity:container==${c}" $WEAVE attach ${weave_cidr} ${c}
