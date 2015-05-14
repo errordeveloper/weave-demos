c02c07ab742c335c9c023cfae1f6e7cae01dbc7b
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-05-14 17:03:13 +0100

    Testing rebased proxy implementation

--- quartet/scripts/defaults.sh
+++ quartet/scripts/defaults.sh
@@ -15,15 +15,12 @@ with_machine_env() {
 create_machine_with_proxy_setup() {
   $DOCKER_MACHINE_CREATE "${1}-${2}"
   eval `$DOCKER_MACHINE env "${1}-${2}"`
-  $DOCKER pull errordeveloper/weaveexec-with-proxy-preview:latest
-  $DOCKER tag errordeveloper/weaveexec-with-proxy-preview:latest zettio/weaveexec:latest
+  for i in weave.tar weaveexec.tar weavedns.tar
+  do $DOCKER load -i ~/Code/weave/$i
+  done
   $WEAVE launch
   $WEAVE launch-dns "10.9.1.${2}/24" -debug
-  $DOCKER run \
-    --privileged -d --name=weaveproxy \
-    -p 12375:12375/tcp -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /proc:/hostproc -e PROCFS=/hostproc \
-    --entrypoint=/home/weave/proxy zettio/weaveexec -debug
+  $WEAVE launch-proxy
 }
 
 create_machine_with_simple_weave_setup() {
