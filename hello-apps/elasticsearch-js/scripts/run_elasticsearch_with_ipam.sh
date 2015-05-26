6b478465f4f58a9d07ca4c82364c1aa334d68f52
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-05-26 19:25:44 +0100

    Remove sudo

--- hello-apps/elasticsearch-js/scripts/run_elasticsearch_with_ipam.sh
+++ hello-apps/elasticsearch-js/scripts/run_elasticsearch_with_ipam.sh
@@ -4,7 +4,7 @@ cd $(git rev-parse --show-toplevel)/felix
 
 for i in 1 2 3 ; do
   hostname="es-${i}.weave.local"
-  cmd="sudo weave run --with-dns --hostname='${hostname}' --name='es' errordeveloper/weave-elasticsearch-minimal:latest"
+  cmd="weave run --with-dns --hostname='${hostname}' --name='es' errordeveloper/weave-elasticsearch-minimal:latest"
   vm="core-0${i}"
   log="/tmp/vagrant_ssh_weave_${vm}"
   echo "Starting ElasticSearch on ${vm}..."
