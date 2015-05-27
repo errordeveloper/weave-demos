1aea3173502cf5a5f0a4c7a34f8bbc857f4dcc20
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-05-27 12:13:28 +0100

    Keep most things like in the original

--- quartet/app/app.py
+++ quartet/app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from redis import Redis
 import os
 app = Flask(__name__)
-redis = Redis(host='redis.weave.local', port=6379)
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
