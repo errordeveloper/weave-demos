38ef9822d85f6b87988b52ae70d3a1a97d1fa52b
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-06-09 11:39:19 +0100

    Remove `DOCKER_MACHINE_CREATE` variable for readability

--- quartet/scripts/1-machine-create.sh
+++ quartet/scripts/1-machine-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 DOCKER_MACHINE_DRIVER=${DOCKER_MACHINE_DRIVER:-"--driver virtualbox"}
-DOCKER_MACHINE_CREATE="docker-machine create ${DOCKER_MACHINE_DRIVER}"
+
 ## I am using curl to create tokens as I find it the easiest, otherwise
 ## one needs to either download or compile a `docker-swarm` binary or
 ## have a Docker daemon running
@@ -12,13 +12,15 @@ swarm_flags="--swarm --swarm-discovery=token://$(${DOCKER_SWARM_CREATE})"
 for i in $(seq 3) ; do
   if [ ${i} = 1 ] ; then
     ## The first machine shall be the Swarm master
-    $DOCKER_MACHINE_CREATE \
+    docker-machine create \
+      ${DOCKER_MACHINE_DRIVER} \
       ${swarm_flags} \
       --swarm-master \
       "weave-${i}"
   else
     ## The rest of machines are Swarm slaves
-    $DOCKER_MACHINE_CREATE \
+    docker-machine create \
+      ${DOCKER_MACHINE_DRIVER} \
       ${swarm_flags} \
       "weave-${i}"
   fi
