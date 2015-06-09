591aeac324671c8ff052144b10d3e456a8aa3cf6
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-06-09 08:22:15 +0100

    Add note about using curl for creating tokens

--- quartet/scripts/1-machine-create.sh
+++ quartet/scripts/1-machine-create.sh
@@ -2,6 +2,9 @@
 
 DOCKER_MACHINE_DRIVER=${DOCKER_MACHINE_DRIVER:-"--driver virtualbox"}
 DOCKER_MACHINE_CREATE="docker-machine create ${DOCKER_MACHINE_DRIVER}"
+## I am using curl to create tokens as I find it the easiest, otherwise
+## one needs to either download or compile a `docker-swarm` binary or
+## have a Docker daemon running
 DOCKER_SWARM_CREATE=${DOCKER_SWARM_CREATE:-"curl -XPOST https://discovery-stage.hub.docker.com/v1/clusters"}
 
 swarm_flags="--swarm --swarm-discovery=token://$(${DOCKER_SWARM_CREATE})"
