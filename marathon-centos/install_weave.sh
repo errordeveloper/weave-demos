68395b31c169ca056ea11206d73e7d306569b44b
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-07-08 07:47:37 +0100

    A fully working setup

--- marathon-centos/install_weave.sh
+++ marathon-centos/install_weave.sh
@@ -4,6 +4,3 @@ cd $(dirname $0)
 
 install -o root -g root -m 0755 weave /usr/local/bin/
 install -o root -g root -m 0644 weave.target weave.service weavedns.service weaveproxy.service /etc/systemd/system/
-
-## Pull container images
-/usr/local/bin/weave setup
