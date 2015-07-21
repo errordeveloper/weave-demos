803df7bccf72b24439863f1498ccbfe455739656
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-07-21 14:45:39 +0100

    Updates for Kubernetes 1.0

--- poseidon/config.rb
+++ poseidon/config.rb
@@ -7,7 +7,7 @@ if File.exists?('kubernetes-cluster.yaml') && ARGV[0].eql?('up')
   token = open('https://discovery.etcd.io/new').read
 
   data = YAML.load(IO.readlines('kubernetes-cluster.yaml')[1..-1].join)
-  data['coreos']['etcd']['discovery'] = token
+  data['coreos']['etcd2']['discovery'] = token
 
   lines = YAML.dump(data).split("\n")
   lines[0] = '#cloud-config'
@@ -20,3 +20,4 @@ end
 $num_instances=3
 $vb_memory = 2048
 $vb_cpus = 2
+$update_channel = 'stable'
